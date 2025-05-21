22f0b92f318266de43f6115cd54ad9f581624b44
Caio Wesley <g8pro.rede@gmail.com>
2025-05-21 10:39:45 -0300

    🔧 Corrige chave do localStorage no AdminRoute (usuarioLogado)

--- src/components/AdminRoute.js
+++ src/components/AdminRoute.js
@@ -4,7 +4,8 @@ import { Navigate } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const AdminRoute = ({ children }) => {
-  const usuario = JSON.parse(localStorage.getItem("usuario"));
+  // Corrigido para buscar a mesma chave usada no login
+  const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
 
   if (!usuario) {
     return <Navigate to="/auth/login" replace />;
