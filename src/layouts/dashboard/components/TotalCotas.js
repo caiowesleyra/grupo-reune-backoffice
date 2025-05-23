5c4c09546e6319720544a24c76887f933d15fa9a
Caio Wesley <g8pro.rede@gmail.com>
2025-05-23 15:57:24 -0300

    atualizacao do total cotas

--- src/layouts/dashboard/components/TotalCotas.js
+++ src/layouts/dashboard/components/TotalCotas.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import axios from "axios";
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
 
 function TotalCotas() {
-  const minhasCotas = 10; // valor temporário
+  const [cotas, setCotas] = useState(0);
+
+  useEffect(() => {
+    const usuario = JSON.parse(localStorage.getItem("usuario"));
+    if (usuario && usuario.id) {
+      axios
+        .get(`https://grupo-reune-backend.onrender.com/api/total-cotas/${usuario.id}`)
+        .then((res) => {
+          setCotas(res.data.total || 0);
+        })
+        .catch((err) => {
+          console.error("Erro ao buscar total de cotas:", err);
+          setCotas(0);
+        });
+    }
+  }, []);
 
   return (
     <MDBox mb={1.5}>
@@ -12,7 +29,7 @@ function TotalCotas() {
         color="warning"
         icon="person_add"
         title="TOTAL DE COTAS"
-        count={minhasCotas}
+        count={cotas}
         percentage={{
           color: "warning",
           amount: "",
