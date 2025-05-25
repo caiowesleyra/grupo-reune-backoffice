8cb0ec0fa068da1fcc0cb6fb7fc463faa05621ed
Caio Wesley <g8pro.rede@gmail.com>
2025-05-25 17:23:42 -0300

    ✅ Ajuste de formatação prettier no botão de voluntário

--- src/layouts/une-social/free-donation/index.js
+++ src/layouts/une-social/free-donation/index.js
@@ -153,7 +153,13 @@ function FreeDonationPage() {
           onChange={(e) => setCidadeEstado(e.target.value)}
         />
 
-        <Button fullWidth variant="contained" color="success" sx={{ mt: 2 }} onClick={handleVoluntario}>
+        <Button 
+          fullWidth
+          variant="contained"
+          color="success"
+          sx={{ mt: 2 }}
+          onClick={handleVoluntario}
+          >
           Enviar Cadastro
         </Button>
         {mensagemVoluntario && (
